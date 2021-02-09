6cb2abc3eaec7b38bbd6916503b645ba9595c084
ZacCarman <zachary.carman@ontariotechu.net>
2021-02-09 17:04:26 -0500

    Basic Files

--- Jeopardy.c
+++ Jeopardy.c
@@ -1,28 +1,58 @@
-#include <stdlib.h>
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <fart.h>
+#include "questions.h"
+#include "players.h"
+#include "jeopardy.h"
+
+
+// Put macros or constants here using #define
+#define BUFFER_LEN 256
+#define NUM_PLAYERS 4
+
+// Put global environment variables here
 struct player{
-string name;
-int cash;
+  string name;
+  int score;
 };
-struct question{
-  string category;
-  string question;
-  string answer;
-  int value;
-  bool correct;
+// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
+void tokenize(char *input, char **tokens);
 
-};
- void initialize_game(void)
+// Displays the game results for each player, their name and final score, ranked from first to last place
+void show_results(player *players, int num_players);
 
- void display_categories(void)
-
- void display_question(char *category, int value)
- bool valid_answer(char *category, int value, char *answer)
- bool already_answered(char *category, int value)
- 
-
-int main(void)
+int main(int argc, char *argv[])
 {
-  printf(""\n", );
+    // An array of 4 players, may need to be a pointer if you want it set dynamically
+    player players[NUM_PLAYERS];
 
+    // Input buffer and and commands
+    char buffer[BUFFER_LEN] = { 0 };
+
+    // Display the game introduction and initialize the questions
+    initialize_game();
+
+    // Prompt for players names
+
+    // initialize each of the players in the array
+
+    // Perform an infinite loop getting command input from users until game ends
+    while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+    {
+        // Call functions from the questions and players source files
+
+        // Execute the game until all questions are answered
+
+        // Display the final results and exit
+    }
+    return EXIT_SUCCESS;
 }
