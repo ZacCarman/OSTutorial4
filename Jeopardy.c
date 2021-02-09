3f54272f324c71afa80b0ca23e4f0c204ee81bac
ZacCarman <zachary.carman@ontariotechu.net>
2021-02-09 15:58:41 -0500

    Simple starting code

---
+++ Jeopardy.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+struct player{
+string name;
+int cash;
+};
+struct question{
+  string category;
+  string question;
+  string answer;
+  int value;
+  bool correct;
+
+};
+ void initialize_game(void)
+
+ void display_categories(void)
+
+ void display_question(char *category, int value)
+ bool valid_answer(char *category, int value, char *answer)
+ bool already_answered(char *category, int value)
+ 
+
+int main(void)
+{
+  printf(""\n", );
+
+}
